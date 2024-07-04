1056dcdca569eeafadb2cfa1706c84364c29d961
MATHEUS DE ALCANTARA FERNANDES <20241148060022@ifrn.local>
2024-07-04 09:12:06 -0300

    Exercico 2 resolvido

---
+++ exercicio_while_2.py
@@ -0,0 +1,8 @@
+resultado = 1
+soma = 0
+while resultado != 0:
+    numero = int(input('Digite valores númericos para a soma:'))
+    soma = soma + numero
+    print(soma)
+    resultado = numero
+print('fim do programa')
